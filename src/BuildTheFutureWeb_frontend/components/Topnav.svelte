0dcea94f773927bfc2df1d1378bfe2947b20ae7c
patnorris <patrick@DESKTOP-TQ4V8LQ.localdomain>
2023-06-02 18:52:22 +0200

    Make top nav bar responsive

--- src/BuildTheFutureWeb_frontend/components/Topnav.svelte
+++ src/BuildTheFutureWeb_frontend/components/Topnav.svelte
@@ -1,4 +1,12 @@
-<div class="flex items-center justify-around flex-wrap bg-slate-600 p-3 text-l font-bold text-white">
+<script>
+  let showMenu = false;
+
+  function toggleMenu() {
+    showMenu = !showMenu;
+  }
+</script>
+
+<div class="topnav flex items-center justify-around flex-wrap bg-slate-600 p-3 text-l font-bold text-white">
   <a href='#'>Future Web</a>
   <a href='#/decentralizedwebstack'>Decentralized Web Stack</a>
   <div class="dropdown">
@@ -10,7 +18,18 @@
   </div>
   <a href='#/relatedinitiatives'>Related Initiatives</a>
   <a href='#/getinvolved'>Get Involved</a>
+  <button class="hamburger" on:click={toggleMenu}>
+    <span class="hamburger-line"></span>
+    <span class="hamburger-line"></span>
+    <span class="hamburger-line"></span>
+  </button>
 </div>
+<nav class="navbar-nav flex items-center justify-around flex-wrap bg-slate-600 p-3 text-l font-bold text-white" class:show={showMenu}>
+  <a href='#/decentralizedwebstack'>Decentralized Web Stack</a>
+  <a href='#/projectsoverview'>Projects</a>
+  <a href='#/relatedinitiatives'>Related Initiatives</a>
+  <a href='#/getinvolved'>Get Involved</a>
+</nav>
 
 <style>
   /* Dropdown Button */
@@ -62,4 +81,58 @@
 .dropdown:hover .dropbtn {
   background-color: #3e8e41;
 }
+
+/* Responsive navigation menu (for mobile devices) */
+@media screen and (max-width: 700px) {
+  .topnav a:not(:first-child), .dropdown .dropbtn {
+    display: none;
+  }
+  .navbar-nav a {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+  }
+}
+
+.hamburger {
+  cursor: pointer;
+  background: none;
+  border: none;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  width: 2rem;
+  height: 2rem;
+  padding: 0;
+  z-index: 10;
+}
+
+.hamburger-line {
+  width: 2rem;
+  height: 0.25rem;
+  background: #fff;
+}
+
+.navbar-nav {
+    display: none;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    transition: transform 0.3s ease-in-out;
+  }
+
+  .navbar-nav.show {
+    display: flex;
+  }
+
+  @media (min-width: 700px) {
+    .hamburger {
+      display: none;
+    }
+
+    .navbar-nav a {
+      display: none;
+    }
+  }
+
 </style>
